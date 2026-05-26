7bac9d6afb1535b83cbe6dae65061b05be6e51ba
decollozq <decollzoq@gmail.com>
2026-05-26 16:08:36 +0900

    feat: TagButton 공통 컴포넌트 개발

---
+++ src/common/TagButton.tsx
@@ -0,0 +1,42 @@
+import type { ButtonHTMLAttributes } from 'react';
+import cn from 'classnames';
+
+interface TagButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: 'primary';
+    selected?: boolean;
+    className?: string;
+}
+
+const variantStyles = {
+    primary: {
+        base: [
+            'border border-main-20 bg-white text-gray-600',
+            'hover:bg-main-10 hover:border-main-30 disabled:bg-main-20 disabled:text-white',
+        ].join(' '),
+        selected: 'border border-main-60 bg-main-50 text-white',
+    },
+};
+
+const TagButton = ({
+    variant = 'primary',
+    selected = false,
+    className,
+    children,
+    ...props
+}: TagButtonProps) => {
+    const variantStyle = variantStyles[variant];
+    return (
+        <button
+            className={cn(
+                'py-2 px-5 text-base font-medium  transition-colors rounded-[20px] cursor-pointer',
+                selected ? variantStyle.selected : variantStyle.base,
+                className,
+            )}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default TagButton;
