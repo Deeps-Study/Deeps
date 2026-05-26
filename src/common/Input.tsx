ff9b79c96077fff6bd974f734c68ffd4462927d6
Aiden0704 <wnsqhs7804@gmail.com>
2026-05-26 18:42:17 +0900

    feat: Input 공통 컴포넌트 구현
    - value를 전달했을 경우, 제어 컴포넌트로 동작하도록 구현
    - value를 전달하지 않았을 경우, 비제어 컴포넌트로 동작하도록 구현
    - 추가적인 기능을 제외하고, 전달되는 Props로 디자인을 결정하도록 구현

---
+++ src/common/Input.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import type { InputHTMLAttributes } from 'react';
+import cn from 'classnames';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    value?: string | number;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+    isFull?: boolean;
+}
+
+const Input = ({
+    value: controlledValue,
+    onChange,
+    onKeyDown,
+    isFull = false,
+    className,
+    children,
+    ...props
+}: InputProps) => {
+    const isControlled = controlledValue !== undefined;
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!isControlled) {
+            setInputValue(e.target.value);
+        }
+        onChange?.(e);
+    };
+
+    const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        onKeyDown?.(e);
+    };
+
+    return (
+        <input
+            className={cn(
+                'py-2 px-3 text-sm font-medium transition-colors rounded-lg bg-white border border-main-20',
+                isFull && 'w-full',
+                className,
+            )}
+            value={isControlled ? controlledValue : inputValue}
+            onChange={handleChange}
+            onKeyDown={handleOnKeyDown}
+            {...props}
+        >
+            {children}
+        </input>
+    );
+};
+
+export default Input;
