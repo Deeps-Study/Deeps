de39aae798654a0288d3a20ce22e95292680d726
Aiden0704 <wnsqhs7804@gmail.com>
2026-06-24 21:14:07 +0900

    feat: NicknamePage 퍼블리싱
    - Nickname을 입력할 수 있는 페이지 퍼블리싱
    - inputConfig를 객체로 구현하여 전달할 Props 구조화

--- src/app/(auth)/nickname/page.tsx
+++ src/app/(auth)/nickname/page.tsx
@@ -1,6 +1,40 @@
+'use client';
+import Input from '@/common/Input';
+import RoundButton from '@/common/RoundButton';
+import { useState } from 'react';
+
+const inputConfig = {
+    id: 'nickname',
+    name: 'nickname',
+    placeholder: '닉네임을 입력해주세요.',
+};
+
 export default function NicknamePage() {
     const [nickname, setNickname] = useState('');
     const isValid = nickname.trim().length >= 2;
+
+    return (
+        <>
+            <div className="w-125 h-78.5 pt-11 pb-14 px-12 border border-main-10 shadow-mint rounded-[40px] bg-white">
+                <form className="flex flex-col h-full justify-between">
+                    <span className="text-main-100 text-xl font-bold text-center">
+                        안녕하세요! 닉네임을 알려주세요
+                    </span>
+
+                    <div className="flex flex-col gap-2">
+                        <p className="text-gray-600 text-base font-bold text-left">
+                            닉네임 입력
+                        </p>
+
+                        <Input
+                            {...inputConfig}
+                            className={`transition-all duration-100 ${isValid ? `shadow-mint` : `shadow-none`}`}
+                            value={nickname}
+                            onChange={(e) => {
+                                setNickname(e.target.value);
+                            }}
+                        ></Input>
+                    </div>
 
                     <RoundButton
                         variant="complete"
@@ -10,3 +44,8 @@ export default function NicknamePage() {
                     >
                         완료
                     </RoundButton>
+                </form>
+            </div>
+        </>
+    );
+}
