9bf59e9966aa441fd3521f9d7a131847a95e0ec7
decollozq <decollzoq@gmail.com>
2026-06-25 20:51:08 +0900

    feat: 목데이터를 이용해 StudyCard 컴포넌트와 CreatCard 컴포넌트를 사용하는 홈화면 구현

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,12 @@
+import MainHeader from '@/common/MainHeader';
+
+export default function HomLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <MainHeader type="home" />
+            <div className="flex flex-1 justify-center  bg-white">
+                {children}
+            </div>
+        </div>
+    );
+}
