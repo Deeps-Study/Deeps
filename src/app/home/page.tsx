03cd7c1cc4cc2f06cc597d414233634ada410d93
decollozq <decollzoq@gmail.com>
2026-06-26 14:00:38 +0900

    feat: 노션의 DB 구조에 맞게 Study interface 변경 및 스터디 상태를 계산하는 util 함수 추기

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,16 +2,17 @@
 import { useState } from 'react';
 import StudyCard from './components/StudyCard';
 import CreateCard from './components/CreateCard';
+import { getStudyStatus } from '@/utils/date';
 
 function HomePage() {
     const [studies, setStudies] = useState<Study[]>([
         {
             id: '1',
             title: '이것도 배우고 저것도 배우는 스터디',
-            status: 'before',
             currentParticipants: 3,
             maxParticipants: 6,
-            period: '2026.05.05 - 2026.06.04',
+            startDate: '2026.05.05',
+            endDate: '2026.06.04',
             tags: ['React', 'TailwindCSS', 'Frontend'],
             description:
                 '안녕하세요 반갑습니다 스터디 설명은 여기에 이렇게 적힙니다. 잘 부탁드려요 호호호',
@@ -19,10 +20,10 @@ function HomePage() {
         {
             id: '2',
             title: '야 너두 백엔드 ㄱ?',
-            status: 'ing',
             currentParticipants: 2,
             maxParticipants: 5,
-            period: '2026.06.01 - 2026.07.01',
+            startDate: '2026.06.01',
+            endDate: '2026.07.01',
             tags: ['Backend', 'SpringBoot'],
             description: '백엔드 정복 스터디입니다.',
         },
@@ -32,6 +33,7 @@ function HomePage() {
     const [isExitPopupOpen, setIsExitPopupOpen] = useState<boolean>(false);
     const [studyToExit, setStudyToExit] = useState<string | null>(null);
     const maxStudyCount = 3;
+
     return (
         <div className="flex flex-col w-screen px-8">
             <h1 className="my-6 text-gray-600 text-center text-3xl font-bold">
@@ -42,7 +44,7 @@ function HomePage() {
                     <StudyCard
                         key={study.id}
                         title={study.title}
-                        status={study.status}
+                        status={getStudyStatus(study.startDate, study.endDate)}
                         currentParticipants={study.currentParticipants}
                         tags={study.tags}
                         onCardClick={() =>
