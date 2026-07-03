54e732a2013dc64b6285193e4f215f8691661721
Aiden0704 <wnsqhs7804@gmail.com>
2026-07-03 21:51:30 +0900

    feat: TimeAlert 인터페이스 추가
    - 시간이 지나면 Alert을 보여주어야 하기에 보여줄지 말지 여부를 정하는 인터페이스 구현

--- src/app/deepsDetail/components/TimeAlert.tsx
+++ src/app/deepsDetail/components/TimeAlert.tsx
@@ -1,12 +1,18 @@
-export function TimeAlert() {
+interface TimeAlertProps {
+    show: boolean;
+}
+
+export function TimeAlert({ show }: TimeAlertProps) {
+    if (!show) return null;
+
     return (
-        <div className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50 w-[575px]">
-            <div className="border border-red-20 bg-red-10 rounded-xl px-10 py-5 text-center flex flex-col gap-1">
-                <p className="text-sm font-bold text-red-100">
-                    제한 시간 종료 전입니다
+        <div className="fixed bottom-20 left-1/2 -translate-x-1/2 z-50 w-[575px] animate-toast">
+            <div className="border border-red-50 bg-red-10 rounded-lg px-10 py-5 text-center flex flex-col gap-1">
+                <p className="text-sm font-bold text-red-200">
+                    제한 시간이 종료되었습니다
                 </p>
-                <p className="text-sm font-medium text-red-100">
-                    제한 시간 종료 후에 다른 멤버들의 답변을 볼 수 있습니다!
+                <p className="text-sm font-bold text-red-200">
+                    다른 멤버들의 답변을 확인해보세요!
                 </p>
             </div>
         </div>
