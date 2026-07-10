e2a8250cf484c352de2b4406e690ed4dee149119
Aiden0704 <wnsqhs7804@gmail.com>
2026-07-10 19:18:00 +0900

    feat: 상단 ProfileCard 컴포넌트 구현
    - 사용자의 이미지, 닉네임을 보여주는 ProfileCard 구현
    - 사용자의 이미지가 없을 시, 기본 이모지 적용하도록 구현

---
+++ src/app/mypage/components/ProfileCard.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import { UserProfileModel } from '@/types/user';
+
+export function ProfileCard({
+    nickname,
+    image,
+    fallbackEmoji,
+}: UserProfileModel) {
+    return (
+        <div className="border border-main-20 rounded-2xl h-[212px] flex flex-col items-center justify-center gap-4 py-5">
+            <div className="flex items-center justify-center size-25 rounded-full border border-main-30 overflow-hidden text-3xl">
+                {image ? (
+                    <Image
+                        src={image}
+                        alt={nickname}
+                        width={100}
+                        height={100}
+                        className="size-full object-cover"
+                    />
+                ) : (
+                    <span>{fallbackEmoji ?? '👤'}</span>
+                )}
+            </div>
+            <p className="text-base font-semibold text-gray-600">{nickname}</p>
+        </div>
+    );
+}
