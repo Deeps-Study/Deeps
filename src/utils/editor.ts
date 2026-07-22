f942bc7c63435e925b0de0d0511e6e9d2dfdbd71
Aiden0704 <wnsqhs7804@gmail.com>
2026-07-22 18:45:29 +0900

    feat: HTML 새니타이징 유틸(sanitizeHtml) 추가

--- src/utils/editor.ts
+++ src/utils/editor.ts
@@ -1,3 +1,5 @@
+import DOMPurify from 'isomorphic-dompurify';
+
 export function isHtmlEmpty(html: string) {
     return (
         html
@@ -5,4 +7,8 @@ export function isHtmlEmpty(html: string) {
             .replace(/&nbsp;/g, '')
             .trim().length === 0
     );
+}
+
+export function sanitizeHtml(html: string): string {
+    return DOMPurify.sanitize(html);
 }
