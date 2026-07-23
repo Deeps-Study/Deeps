51b27e8cd4e26abb9456a76139dcd02f4f6daeab
decollozq <decollzoq@gmail.com>
2026-07-23 19:30:09 +0900

    fix: 문제 출제자를 찾지 못하는 경우 발생하는 에러 수정 및 UI 변경

--- src/app/study/components/DeepsCard.tsx
+++ src/app/study/components/DeepsCard.tsx
@@ -62,46 +62,43 @@ export default function DeepsCard({
     return (
         <div
             onClick={() => router.push(`/deepsDetail/${studyId}/${id}`)}
-            className="flex flex-col gap-2 rounded-lg border border-main-20 bg-white px-3 py-2 shadow-sm hover:shadow-mint hover:cursor-pointer transition-all"
+            className="flex flex-col gap-2 rounded-lg border border-main-20 bg-white px-3 py-3 shadow-sm hover:shadow-mint hover:cursor-pointer transition-all"
         >
-            <div className="border-b border-main-20 py-2">
-                <div className="flex items-start gap-2.5">
-                    {/* 작성자 아바타 */}
-                    <div className="flex h-10 w-10 items-center justify-center rounded-full border border-main-20 bg-white text-base font-bold overflow-hidden select-none">
-                        {creator.image ? (
-                            <Image
-                                src={creator.image}
-                                alt={creator.nickname}
-                                width={36}
-                                height={36}
-                                className="h-full w-full object-cover"
-                            />
-                        ) : (
-                            <Icon
-                                name="user"
-                                className="w-4 h-4 text-main-100 stroke-2 fill-current"
-                            />
-                        )}
-                    </div>
-                    <div className="flex flex-1 flex-col gap-1 px-1.5">
-                        <p className="text-base font-bold text-gray-600">
-                            {title}
-                        </p>
-                        <div className="flex items-center gap-1">
-                            <Icon
-                                name="user"
-                                className="h-3 w-3 text-main-200 stroke-2"
-                            />
-                            <span className="text-xs font-medium text-gray-600">
-                                {creator.nickname}
-                            </span>
-                        </div>
-                    </div>
+            <div className="flex items-center gap-2.5 border-b border-main-20 pb-3">
+                {/* 작성자 프로필사진 */}
+                <div className="flex h-10 w-10 items-center justify-center rounded-full border border-main-20 bg-white text-base font-bold overflow-hidden select-none">
+                    {creator?.image ? (
+                        <Image
+                            src={creator.image}
+                            alt={creator.nickname ?? '작성자 프로필'}
+                            width={36}
+                            height={36}
+                            className="h-full w-full object-cover"
+                        />
+                    ) : (
+                        <Icon
+                            name="user"
+                            className="w-4 h-4 text-main-100 stroke-2 fill-current"
+                        />
+                    )}
+                </div>
+                {/* 딥스 제목 */}
+                <div className="flex flex-1 px-1.5">
+                    <p className="text-base font-bold text-gray-600">{title}</p>
                 </div>
             </div>
 
             <div className="flex items-center justify-between px-1.5 py-2">
                 <div className="flex items-center gap-5">
+                    <div className="flex items-center gap-1">
+                        <Icon
+                            name="user"
+                            className="h-3 w-3 text-main-200 stroke-2"
+                        />
+                        <span className="text-xs font-medium text-gray-600">
+                            {creator?.nickname ?? '탈퇴한 사용자'}
+                        </span>
+                    </div>
                     {/* 카운트다운 타이머 */}
                     <div className="flex items-center gap-2">
                         <Icon name="clock" className="h-4 w-4 text-main-200" />
